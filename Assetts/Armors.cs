9408efe077387f6eb40bfd0728de044a380b07fb
Gregory Markus <gregmarkus1992@yahoo.com>
2024-01-22 18:28:49 -0600

    Modularizing everything for cleaner use. got started on some logic

---
+++ Assetts/Armors.cs
@@ -0,0 +1,33 @@
+﻿using GRPG.Models;
+
+
+namespace GRPG.Assetts
+{
+    public class Armors
+    {
+        List<Armor> armors = new()
+            {
+            new Armor()
+            {
+                Id = 1,
+                Name = "Chain Mail",
+                AttackSpeedModifier = -1,
+                DefenseModifier = 3,
+            },
+            new Armor()
+            {
+                Id = 2,
+                Name = "Silk Robe",
+                AttackSpeedModifier = 2,
+                DefenseModifier = 0,
+            },
+            new Armor()
+            {
+                Id = 3,
+                Name = "Leather Armor",
+                AttackSpeedModifier = 1,
+                DefenseModifier = 1,
+            },
+            };
+    }
+}
